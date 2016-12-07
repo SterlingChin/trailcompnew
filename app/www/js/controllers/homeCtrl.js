bade58a0210d584cc95fc41a082c2a490116bf9f
Sterling Chin <sterling.chin@gmail.com>
2016-12-07 10:32:38 -0700

    testing home view 2

--- app/www/js/controllers/homeCtrl.js
+++ app/www/js/controllers/homeCtrl.js
@@ -1,18 +1,32 @@
-angular.module('trail').controller('homeCtrl', function ($scope, $cordovaGeolocation, mainSvc) {
-  var geo = $cordovaGeolocation.getCurrentPosition({frequency: 3000, timeout: 5000, enableHighAccuracy: true});
+angular.module('trail').controller('homeCtrl', function ($scope, $cordovaGeolocation, $ionicLoading, mainSvc) {
+  var geo = $cordovaGeolocation.getCurrentPosition({ frequency: 3000, timeout: 5000, enableHighAccuracy: true});
 
-  $scope.gpsHomePing = function () {
+  // $scope.gpsHomePing = function () {
+  //   geo.then(function (position) {
+  //       $scope.lat = position.coords.latitude;
+  //       $scope.long = position.coords.longitude;
+  //     },
+  //     function error(err) {
+  //       $scope.errors = err;
+  //     }
+  //   )
+  // };
+
+  $scope.updateWeather = function () {
+    $scope.loading = $ionicLoading.show({
+      content: 'Getting current location...',
+      showBackdrop: false
+    });
+    setTimeout(getWeather, 1000);
+    setTimeout(getForecast, 1000);
     geo.then(function (position) {
         $scope.lat = position.coords.latitude;
         $scope.long = position.coords.longitude;
+        $scope.loading.hide();
       },
       function error(err) {
         $scope.errors = err;
-      }
-    )
-  };
-
-  $scope.updateWeather = function () {
+      });
 
     mainSvc.getWeather($scope.lat, $scope.long).then(function (weatherObject) {
       $scope.weatherTemp = weatherObject.currentTemp;
