12d8813cda97208e1009c5a3a3efdd1453b385fa
Sterling Chin <sterling.chin@gmail.com>
2016-12-06 17:46:51 -0700

    Changing Home View

--- app/www/js/controllers/homeCtrl.js
+++ app/www/js/controllers/homeCtrl.js
@@ -1,33 +1,33 @@
 angular.module('trail').controller('homeCtrl', function($scope, mainSvc) {
+   $scope.gpsHomePing = function () {
+    var geoSettings = {
+      frequency: 3000,
+      timeout: 5000,
+      enableHighAccuracy: true
+    };
+
+    var geo = $cordovaGeolocation.getCurrentPosition(geoSettings);
+
+    geo.then(function (position) {
+        $scope.lat =  position.coords.latitude;
+        $scope.long = position.coords.longitude;
+
+      },
+      function error(err) {
+        $scope.errors = err;
+      }
+    );
+  };
  $scope.updateWeather = function(){
 
-  // $scope.gpsPing = function () {
-  //   var geoSettings = {
-  //     frequency: 3000,
-  //     timeout: 5000,
-  //     enableHighAccuracy: true
-  //   };
-
-  //   var geo = $cordovaGeolocation.getCurrentPosition(geoSettings);
-
-  //   geo.then(function (position) {
-  //       $scope.lat =  position.coords.latitude;
-  //       $scope.long = position.coords.longitude;
-
-  //     },
-  //     function error(err) {
-  //       $scope.errors = err;
-  //     }
-  //   );
-  // };
-    mainSvc.getWeather(mainSvc.location.lat, mainSvc.location.long).then(function(weatherObject) {
+    mainSvc.getWeather($scope.lat, $scope.long).then(function(weatherObject) {
     // mainSvc.getWeather().then(function(weatherObject) {
         $scope.weatherTemp = weatherObject.currentTemp;
         $scope.weatherDesc = weatherObject.currentDesc;
         $scope.weatherHum = weatherObject.currentHum;
         $scope.weatherSpeed = weatherObject.currentWindSpeed;
     });
-    mainSvc.getForecast(mainSvc.location.lat, mainSvc.location.long).then(function(forecastObject) {        
+    mainSvc.getForecast($scope.lat, $scope.long).then(function(forecastObject) {        
     // mainSvc.getForecast().then(function(forecastObject) { 
         $scope.forecastTempHigh = forecastObject.tempHigh;
         $scope.forecastTempLow = forecastObject.tempLow;
