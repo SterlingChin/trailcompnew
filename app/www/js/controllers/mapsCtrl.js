48377ca11ea4a4cc8639aab50f647504c6d7dca7
Sterling Chin <sterling.chin@gmail.com>
2016-12-06 17:39:06 -0700

    Maps2

--- app/www/js/controllers/mapsCtrl.js
+++ app/www/js/controllers/mapsCtrl.js
@@ -1,28 +1,51 @@
-angular.module('trail').controller('mapsCtrl', function($scope, $ionicLoading, mainSvc){
-  $scope.mapCreated = function(map) {
+angular.module('trail').controller('mapsCtrl', function ($scope, $ionicLoading, mainSvc) {
+  $scope.mapCreated = function (map) {
     $scope.map = map;
   };
+  var geoSettings = {
+      frequency: 3000,
+      timeout: 5000,
+      enableHighAccuracy: true
+    };
+
   // $scope.getLocation = function () {
   //   $scope.location = mainSvc.gpsPingSvc();
   // }
 
-  // $scope.centerOnMe = function () {
-  //   console.log("Centering");
-  //   if (!$scope.map) {
-  //     return;
-  //   }
+  $scope.centerOnMe = function () {
+    console.log("Centering");
+    if (!$scope.map) {
+      return;
+    }
 
-  //   $scope.loading = $ionicLoading.show({
-  //     content: 'Getting current location...',
-  //     showBackdrop: false
-  //   });
+    $scope.loading = $ionicLoading.show({
+      content: 'Getting current location...',
+      showBackdrop: false
+    });
 
-  //   navigator.geolocation.getCurrentPosition(function (pos) {
-  //     console.log('Got pos', pos);
-  //     $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-  //     $scope.loading.hide();
-  //   }, function (error) {
-  //     alert('Unable to get location: ' + error.message);
-  //   });
-  // };
+    $cordovaGeolocation.getCurrentPosition(function (pos) {
+      console.log('Got pos', pos);
+      $scope.map.setCenter(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+      $scope.loading.hide();
+    }, function (error) {
+      alert('Unable to get location: ' + error.message);
+    });
+  };
+  google.maps.event.addListenerOnce($scope.map, 'idle', function () {
+
+    var marker = new google.maps.Marker({
+      map: $scope.map,
+      animation: google.maps.Animation.DROP,
+      position: latLng
+    });
+
+    var infoWindow = new google.maps.InfoWindow({
+      content: "Here I am!"
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+      infoWindow.open($scope.map, marker);
+    });
+
+  });
 });
